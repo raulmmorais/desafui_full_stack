61f78850d7357c6ec6d241c37b1e8dd4c591adfe
Raul de Melo Morais <raulmmorais@gmail.com>
2019-04-16 11:53:22 -0300

    Update tarefas-service.service.ts

--- frontend/src/app/tarefas-service.service.ts
+++ frontend/src/app/tarefas-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarefasService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+  
+  pesquisarTarefas(): Observable<Response>{
+    const url = 'localhost:8080/tarefas';
+    return this.http.get(url);
+  }
 }
